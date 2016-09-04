551719c5e13fa31fbc1c52c1fe8bb71719ab628c
Cheick <cab25004@vt.edu>
2016-09-04 16:51:47 -0400

    cleaned main

--- src/Memman.java
+++ src/Memman.java
@@ -40,36 +40,7 @@ public class Memman {
      *     Command line parameters
      */
     public static void main(String[] args) {
-        // This is the main file for the program.
-    	 String fileName = "P1sampleInput.txt";
-         File file = new File(fileName);
-         Scanner reader = null;
-
-         try {
-             reader = new Scanner(file);
-
-             String token = reader.nextLine();
-
-             int offset = token.length()+1;
-
-             //System.out.println(reader.nextLine());
-
-             while(reader.hasNextLine()) {
-
-                 token = reader.nextLine();
-
-                 String[] line = token.split(" ");
-             
-             }
-
-
-             reader.close();
-
-
-         } catch (IOException exception) {
-             exception.printStackTrace();
-         }
-
-     }
+    
+    }
     
 }
