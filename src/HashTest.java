597a4a39ad5812279a5546062518a697427cfbb2
Broulaye <broulaye@vt.edu>
2016-09-05 17:37:05 -0400

    styling fixed

--- src/HashTest.java
+++ src/HashTest.java
@@ -1,3 +1,4 @@
+
 import student.TestCase;
 
 // -------------------------------------------------------------------------
@@ -9,11 +10,19 @@ import student.TestCase;
  *  @version 09/05/2016
  */
 public class HashTest extends TestCase {
-    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String ALPHA_NUMERIC_STRING = 
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
+    /**
+     * this method generate random strings
+     * @param count represent the string length
+     * @return a random string 
+     */
     public static String randomAlphaNumeric(int count) {
         StringBuilder builder = new StringBuilder();
         while (count-- != 0) {
-            int character = (int)(Math.random() * ALPHA_NUMERIC_STRING.length());
+            int character = (int)(Math.random() * 
+                    ALPHA_NUMERIC_STRING.length());
             builder.append(ALPHA_NUMERIC_STRING.charAt(character));
         }
         return builder.toString();
@@ -37,16 +46,26 @@ public class HashTest extends TestCase {
                 myHash.insertString(randomAlphaNumeric(Math.round(6)));
             }
             myHash.printTable();
-        } catch (Exception e) {
+        } 
+        catch (Exception e) {
+            assertTrue(e instanceof Exception);
             e.printStackTrace();
         }
         
     }
+    
+    /**
+     * Test the hash table with negative size
+     */
     public void testNegativeSize() {
+        
         Hash table;
         try {
             table = new Hash(-2);
-        } catch (Exception e) {
+            table.insertString("fdf");
+        } 
+        catch (Exception e) {
+            assertTrue(e instanceof Exception);
             e.printStackTrace();
         }
     }
--- src/Parser.java
+++ src/Parser.java
@@ -115,7 +115,7 @@ public class Parser {
                         System.out.println("Illegal Operation");
                         break;
 
-                    }
+                }
 
                 commandsList.add(command);
 
