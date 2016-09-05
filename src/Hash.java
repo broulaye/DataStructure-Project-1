4cb25b04d6bd31087108f617cbc64c887e444f6c
Cheick <cab25004@vt.edu>
2016-09-05 03:53:20 -0400

    added insert and print methods in hash class

--- src/Hash.java
+++ src/Hash.java
@@ -1,23 +1,78 @@
 /**
- * Stub for hash table class
+ * Hash table class.
  *
- * @author CS3114 staff
- * @version August 27, 2016
+ * @author Cheick Berthe
+ * @version Sep 5, 2016
  */
 
-public class Hash
-{
+public class Hash {
 
     /**
      * Create a new Hash object.
-
+     * @throws Exception thrown when negative size value is passed
      */
-    public Hash()
-    {
-        // Nothing here yet
+    public Hash(int initialSize) throws Exception {
+    	if (initialSize <= 0) {
+    		throw new Exception("WARN: invalid size type");
+    	}
+        valueArray = new String[initialSize];
+        numbElements = 0;
     }
-
-
+    /**
+     * 
+     * @param arr (array to be resized)
+     * @param newSize (new size)
+     * @return new array
+     */
+    private String[] resizeArray(String[] arr, int newSize) {
+    	String[] newArray = new String[newSize];
+    	for (int it = 0; it < arr.length;  it++) {
+    		if (arr[it] != null) {
+    			newArray[it] = new String(arr[it]);
+    		}
+    	}
+    	return newArray;
+    }
+    
+    // Array of strings
+    private String[] valueArray;
+    // number of elements in table
+    private int numbElements;
+    /**
+     * 
+     * @param str (string to insert)
+     * @return position of insertion
+     * @throws Exception when all possible slots have been proved and are occupied
+     */
+    public int insertString(String str) throws Exception {
+    	int index;
+    	int pos = index = hash(str, valueArray.length);
+    	int i = 0;
+    	while (valueArray[pos] != null) {
+    		pos = (pos + ++i * i) % valueArray.length;
+    		if (pos == index) {
+    			throw new Exception("WARN: Cannot insert string");
+    		}
+    	}
+        valueArray[pos] = new String(str);
+        numbElements++;
+        if (numbElements >= (valueArray.length >> 1)) {
+        	valueArray = resizeArray(valueArray, valueArray.length * 2);
+        }
+       	return index;
+    }
+    /**
+     * 
+     */
+    public void printTable() {
+    	for (String string : valueArray) {
+			if (string != null) {
+				System.out.println(string);
+			} else { 
+				System.out.println("**NULL**");
+			}
+		}
+    }
     /**
      * Compute the hash function. Uses the "sfold" method from the OpenDSA
      * module on hash functions
@@ -28,19 +83,15 @@ public class Hash
      *            The size of the hash table
      * @return The home slot for that string
      */
-    // Make this private in your project.
     // This is private for distributing hash function in a way that will
     // pass milestone 1 without change.
-    public int h(String s, int m)
-    {
+    private int hash(String s, int m) {
         int intLength = s.length() / 4;
         long sum = 0;
-        for (int j = 0; j < intLength; j++)
-        {
+        for (int j = 0; j < intLength; j++) {
             char[] c = s.substring(j * 4, (j * 4) + 4).toCharArray();
             long mult = 1;
-            for (int k = 0; k < c.length; k++)
-            {
+            for (int k = 0; k < c.length; k++) {
                 sum += c[k] * mult;
                 mult *= 256;
             }
@@ -48,8 +99,7 @@ public class Hash
 
         char[] c = s.substring(intLength * 4).toCharArray();
         long mult = 1;
-        for (int k = 0; k < c.length; k++)
-        {
+        for (int k = 0; k < c.length; k++) {
             sum += c[k] * mult;
             mult *= 256;
         }
--- src/HashTest.java
+++ src/HashTest.java
@@ -4,24 +4,41 @@ import student.TestCase;
 /**
  *  Test the hash function (you should throw this away for your project)
  *
- *  @author CS3114 staff
- *  @version Aug 27, 2016
+ *  @author Cheick Berthe
+ *  @version Sep 5, 2016
  */
 public class HashTest extends TestCase {
+	private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    public static String randomAlphaNumeric(int count) {
+        StringBuilder builder = new StringBuilder();
+        while (count-- != 0) {
+            int character = (int)(Math.random() * ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+        }
+        return builder.toString();
+    }
     /**
      * Sets up the tests that follow.
      */
     public void setUp() {
         // Nothing Here
     }
-
+    
     /**
-     * Test the hash function
+     * Test the hash table
      */
     public void testh() {
 
-        Hash myHash = new Hash();
-        assertEquals(myHash.h("aaaabbbb", 101), 75);
-        assertEquals(myHash.h("aaaabbb", 101), 1640219587 % 101);
+        Hash myHash;
+		try {
+			myHash = new Hash(2);
+			for (int i = 0; i < 20; i++) {
+				myHash.insertString(randomAlphaNumeric(Math.round(6)));
+			}
+	        myHash.printTable();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
     }
 }
