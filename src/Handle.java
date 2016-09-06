5fb52c1867161e74bd0192163e6b70a98d8af898
cheickmec <cab25004@vt.edu>
2016-09-05 20:40:30 -0400

    started memory manager

---
+++ src/Handle.java
@@ -0,0 +1,5 @@
+/**
+ * Created by berth on 9/5/2016.
+ */
+public class Handle {
+}
---
+++ src/MemManager.java
@@ -0,0 +1,65 @@
+/**
+ * @author Cheick Berthe
+ * @author Broulaye Doumbia
+ * @version 9/5/2016.
+ */
+public class MemManager {
+    // Freeblock list
+    private DLLinkedList<byte> freeBlockList;
+    // Artist Hash Table
+    private Hash artistTable;
+    // Song Hash Table
+    private Hash songTable;
+    /**
+     * constructor
+     * @param poolsize size of memory pool in bytes
+     */
+    public MemManager(int poolsize) {
+        freeBlockList = new DLLinkedList<>();
+        try {
+            artistTable = new Hash(poolsize);
+            songTable = new Hash(poolsize);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Insert a record and return its position handle
+     * @param space record to be inserted
+     * @param size length of the record
+     * @return
+     */
+    public Handle insert(byte[] space, int size) {
+        return null;
+    }
+
+    /**
+     * Free a block at position specified by theHandle
+     * Merge adjacent blocks
+     * @param theHandle handle referring to position
+     */
+    public void remove(Handle theHandle) {
+        // TODO:
+    }
+
+    /**
+     * Return the record with given handle up to size
+     * bytes by copying it into space
+     * @param space returned record
+     * @param theHandle handle to be retrieved
+     * @param size size of record to be returned
+     * @return record with
+     */
+    public int get(byte[] space, Handle theHandle, int size) {
+        // TODO
+        return 0;
+    }
+
+    // Dump a printout of the freeblock list
+    void dump() {
+        freeBlockList.toString();
+    }
+}
